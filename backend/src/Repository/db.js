9a81c13c86ab980c52df529cb46d767c896a2931
guilh@Leal <guilhermeleals776@gmail.com>
2025-11-03 09:49:53 -0300

    alterado guilherme 03/11

--- backend/src/Repository/db.js
+++ backend/src/Repository/db.js
@@ -1,5 +1,6 @@
 import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
+import process from 'process';
 
 dotenv.config();
 
@@ -9,8 +10,8 @@ const env = process.env;
 const dbConfig = {
   host: env.DB_HOST || 'localhost',
   user: env.DB_USER || 'root',
-  password: env.DB_PASSWORD || 'enzogaeta17',
-  database: env.DB_NAME || 'xestudos',
+  password: env.DB_PASSWORD || 'GuiiLherme0091',
+  database: env.DB_NAME || 'MindUP',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
